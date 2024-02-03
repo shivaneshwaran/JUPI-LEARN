78a977c3121af339e5af500296b2cc88428e67bd
S SHIVANESHWARAN <139312889+shivaneshwaran@users.noreply.github.com>
2024-02-03 22:34:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def home():
     return render_template("JUPILEARNINGWEBAPP.HTML")
 
 @app.route('/about')
-def aboutus():
+def about():
     return render_template("about.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
