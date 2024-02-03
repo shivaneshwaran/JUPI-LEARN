742568ed1b044d48d2a499e4ce350a6f7cd38d1a
S SHIVANESHWARAN <139312889+shivaneshwaran@users.noreply.github.com>
2024-02-03 22:36:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # !/usr/bin/python
-from flask import Flask, render_template
+from flask import Flask, render_templates
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("JUPILEARNINGWEBAPP.HTML")
+    return render_templates("JUPILEARNINGWEBAPP.HTML")
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_templates("about.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
