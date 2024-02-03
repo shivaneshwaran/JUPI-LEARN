be60b23763dc8a42f3a1d5b691ed3d816cdf043f
gouthamsanthosh-gsx <gouthamsanthosh.xyz@gmail.com>
2024-02-03 19:58:21 +0530

    modified:   app.py 	deleted:    templates/Naming.txt

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('HOMEPAGE.html')
+   return render_template("HOMEPAGE.html")
 if __name__ == '__main__':
    app.run(host="0.0.0.0",port=80,debug=True)
