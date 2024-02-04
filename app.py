f03037c00914c73b00af44744689a7e0d50114fc
gouthamsanthosh-gsx <gouthamsanthosh.xyz@gmail.com>
2024-02-04 17:58:35 +0530

    modified:   app.py 	deleted:    assets/images/download (2).jpg 	deleted:    assets/images/gradient-network-connection-background_23-2148871843.avif 	deleted:    assets/images/istockphoto-1409522268-612x612.jpg 	deleted:    assets/images/non-copyrighted-purple-space-giwyzcnvy9m2hvhm.jpg 	deleted:    assets/images/wavy-background-concept_23-2148497712.avif 	renamed:    assets/images/unknown_user.png -> static/images/unknown_user.png 	modified:   templates/JUPILEARNINGWEBAPP.HTML 	modified:   templates/about.html

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static")
 
 @app.route('/')
 def home():
     return render_template("JUPILEARNINGWEBAPP.HTML")
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return render_template("about.html")
 
