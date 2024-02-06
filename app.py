a04fc2f7b5ebc4269e59283dfd71d02695652124
S SHIVANESHWARAN <139312889+shivaneshwaran@users.noreply.github.com>
2024-02-06 16:51:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def login():
     return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=500, debug=True)
