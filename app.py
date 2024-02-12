d640613c9ee369f7fee62e7869ad7d5471e42a9e
gouthamsanthosh-gsx <gouthamsanthosh.xyz@gmail.com>
2024-02-12 18:24:52 +0530

    modified:   app.py 	renamed:    index.html -> templates/index.html 	modified:   templates/login.html 	new file:   templates/signup.html

--- app.py
+++ app.py
@@ -5,23 +5,27 @@ app = Flask(__name__,static_folder="static")
 
 @app.route('/')
 def home():
-    return render_template("JUPILEARNINGWEBAPP.HTML")
+	return render_template("index.html")
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+	return send_from_directory(path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+	return render_template("about.html")
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+	return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+	return render_template("signup.html")
 
 @app.route("/course")
 def course():
-    return render_template("frontendai.html")
+	return render_template("frontendai.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+	app.run(host="0.0.0.0", port=80, debug=True)
