f15a0867e65d0e3333fcdac386d14ead2af74400
S SHIVANESHWARAN <shivaneshwaran10@gmail.com>
2024-02-20 22:03:16 +0530

    main update fix

--- app.py
+++ app.py
@@ -1,66 +1,58 @@
-from flask import Flask, render_template, render_template_string, send_from_directory, request, make_response, redirect, jsonify
+from flask import Flask,render_template,render_template_string,send_from_directory,request,make_response,redirect
 from os import path
-import requests
 import backend
+import requests
+app = Flask(__name__,static_folder="static")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-app = Flask(__name__, static_folder="static")
 
 def error_msg(msg):
-    return render_template_string("<script>alert('Error: {}');window.history.back();</script>".format(msg))
+	return render_template_string("<script>alert('Error: {}');window.history.back();</script>".format(msg))
 
-def display(template, username="", course="Nothing"):
-    response = make_response(render_template(template, USERNAME=username, COURSE=course))
-    response.headers["Cache-Control"] = "no-cache, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    if request.cookies.get("SESSIONID") is None:
-        response.set_cookie("SESSIONID", value="")
-    return response
+def display(template,username="",course="Nothing"):
+	response = make_response(render_template(template,USERNAME=username,COURSE=course))
+	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	response.headers["Pragma"] = "no-cache"
+	response.headers["Expires"] = "0"
+	if request.cookies.get("SESSIONID") is None:
+		response.set_cookie("SESSIONID",value="")
+	return response
 
 def set_auth_token(token):
-    response = make_response(redirect("/course"))
-    response.set_cookie("SESSIONID", value=token)
-    return response
+	response = make_response(redirect("/course"))
+	response.set_cookie("SESSIONID",value=token)
+	return response
+
+
 
 '''Static page rendering'''
-
-    
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
+	return send_from_directory(path.join(app.root_path, "static"),"favicon.ico", mimetype="image/vnd.microsoft.icon")
 
 @app.route('/')
 def home():
-    return display("index.html")
+	return display("index.html")
 
 @app.errorhandler(404)
 def not_found(e):
-    return render_template("404.html")
+  return render_template("404.html")
 
 @app.route("/about")
 def about():
-    return display("about.html")
+	return display("about.html")
 
 @app.route("/login")
 def login():
-    if backend.validate_token(request.cookies.get("SESSIONID"))[0]:
-        return redirect("/course")
-    else:
-        return display("login.html")
+	if backend.validate_token(request.cookies.get("SESSIONID"))[0]:
+		return redirect("/course")
+	else:
+		return display("login.html")
 
 @app.route("/signup")
 def signup():
-    return display("signup.html")
-
-
-
-@app.route("/logout")
-def logout():
-    response = make_response(redirect("/login"))
-    response.set_cookie("SESSIONID", value="")
-    return response
-
+	return display("signup.html")
 
 @app.route("/course", methods=["POST", "GET"])
 def course():
@@ -68,12 +60,12 @@ def course():
         course = request.form["course"]
     except:
         course = "Nothing"
-        
+    
     validated, username = backend.validate_token(request.cookies.get("SESSIONID"))
     
     if validated:
         # Make a request to the frontend server to fetch frontend.html
-        frontendai_url = "http://34.67.83.7:5000"
+        frontendai_url = "http://127.0.0.1:5000"
         response = requests.get(frontendai_url)
         
         if response.status_code == 200:
@@ -86,7 +78,29 @@ def course():
         return redirect("/404")
 
 
+@app.route("/logout")
+def logout():
+	response = make_response(redirect("/login"))
+	response.set_cookie("SESSIONID",value="")
+	return response
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1983, debug=True)
+
+'''Handling Authentication'''
+@app.route("/api_signup", methods=["POST"])
+def api_signup():
+	validated,message = backend.validate_signup(request.form)
+	if validated:
+		return render_template_string("<script>alert('Account was successfully created!');window.location.href='/login';</script>")
+	else:
+		return error_msg(message)
+
+@app.route("/api_signin", methods=["POST"])
+def api_signin():
+	authenticated,token = backend.signin_account(request.form)
+	if authenticated:
+		return set_auth_token(token)
+	else:
+		return error_msg("Invalid username or password!")
+
+app.run(host="0.0.0.0", port=80, debug=True)
--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
-# JUPI-LEARN Backend - Created by [ S SHIVANESHWARAN ]
-
+# !/usr/bin/python
+'''JUPI-LEARN backend code '''
 import mysql.connector as mys
 import os
 import re
@@ -7,168 +7,203 @@ import hashlib
 import random
 import smtplib
 from cryptography.fernet import Fernet
-from dotenv import load_dotenv
-import os
 
-# Global variables
-load_dotenv()
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DB")
-SECRET_KEY = os.getenv("SECRET_KEY")
-MAIL_ID = os.getenv("MAIL_ID")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+env = {}
+try:
+	with open(".env") as f:
+		while True:
+			x = f.readline()
+			if x:
+				x = x.strip()
+				if not x.startswith("#"):
+					key,value = x.split("=")
+					env[key] = value
+			else:
+				break
+except:
+	print("Failed to read env file")
+	os._exit(0)
 
-# Function to initialize MySQL database
+
+#Global variables and configuation(from env file)
+MYSQL_HOST = env["MYSQL_HOST"]
+MYSQL_USER = env["MYSQL_USER"]
+MYSQL_PASSWORD = env["MYSQL_PASSWORD"]
+MYSQL_DB = env["MYSQL_DB"]
+SECRET_KEY = (env["SECRET_KEY"] + "=").encode()
+MAIL_ID = env["MAIL_ID"]
+MAIL_PASSWORD = env["MAIL_PASSWORD"]
+
 def mysqlDB_init():
-    dbExists = False
-    try:
-        con = mys.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD)
-        cur = con.cursor()
-        cur.execute("show databases;")
-        for db in cur:
-            if db[0] == MYSQL_DB:
-                dbExists = True
-                break
-        if not dbExists:
-            cur.execute("create database {};".format(MYSQL_DB))
-        con.close()
-    except:
-        print("MySQL connection failed!")
-        os._exit(0)
+	'''Create mysql db if it doesn't exist'''
+	dbExists = False
+	try:
+		con = mys.connect(host=MYSQL_HOST,user=MYSQL_USER,password=MYSQL_PASSWORD)
+		cur = con.cursor()
+		cur.execute("show databases;")
+		for db in cur:
+			if db[0] == MYSQL_DB:
+				dbExists = True
+				break
+		if not dbExists:
+			cur.execute("create database {};".format(MYSQL_DB))
+		con.close()
+	except:
+		print("MySQL connection failed!")
+		os._exit(0)
 
-# Main code starts here
-# Initialize encryption
+
+
+#<Main code starts here>
+#Initialize encryption
 fernet = Fernet(SECRET_KEY)
 
-# Generate salt for hashing password
 def gen_salt(length):
-    charSet = "abcdefghijklmnopqrstuvwxyz1234567890"
-    salt = ""
-    for i in range(length):
-        r1 = random.randint(0, len(charSet) - 1)
-        c = charSet[r1]
-        if c.isalpha():
-            r2 = random.randint(0, 1)
-            if r2 == 0:
-                salt += c
-            else:
-                salt += c.upper()
-        else:
-            salt += c
-    return salt
+	'''Generate salt for hashing password'''
+	charSet = "abcdefghijklmnopqrstuvwxyz1234567890"
+	salt = ""
+	for i in range(length):
+		r1 = random.randint(0,len(charSet)-1)
+		c = charSet[r1]
+		if c.isalpha():
+			r2 = random.randint(0,1)
+			if r2 == 0:
+				salt += c
+			else:
+				salt += c.upper()
+		else:
+			salt += c
+	return salt
 
-# Connect to MySQL server and create users table if not exists
+
+#Connect to mysql server
 mysqlDB_init()
 
 try:
-    con = mys.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, database=MYSQL_DB)
-    if con.is_connected():
-        print("MySQL connection established...")
+	con = mys.connect(host=MYSQL_HOST,user=MYSQL_USER,password=MYSQL_PASSWORD,database=MYSQL_DB)
+	if con.is_connected():
+		print("MySQL connection established...")
 except Exception as e:
-    print("MySQL Connection error")
+	print("MySQL Connection error")
 
 cur = con.cursor()
 
+
+#Creating users table
+#The table user has these fields by default(You may add/remove/modify fields or properties)
+#	User ID - auto incrementing value
+#	Name of user - 256 characters
+#	Email - 320 characters
+#	Password - stored as hash(sha-256)
+#	Salt - 32 char
+#	Prompt - 3000 char limit(to store prompts from user)
+
 try:
-    cur.execute("create table users(user_id int primary key auto_increment, name varchar(256), email varchar(320), password char(64), salt char(32), prompt varchar(3000));")
+	cur.execute("create table users(user_id int primary key auto_increment, name varchar(256),email varchar(320), password char(64), salt char(32), prompt varchar(3000));")
 except:
-    pass
+	pass
 
-# Function to retrieve data from users table
 def db():
-    db = []
-    cur.execute("SELECT * FROM users;")
-    for i in cur:
-        db.append(i)
-    return db
+	db = []
+	cur.execute("select * from users;")
+	for i in cur:
+		db.append(i)
+	return db
 
-# Function to send welcome mail to new users
-def send_mail(address, name):
-    try:
-        mailServer = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        mailServer.ehlo()
-        mailServer.login(MAIL_ID, MAIL_PASSWORD)
-    except:
-        print("Failed connecting to Gmail server")
-    try:
-        mSubject = "Welcome to JUPI-Learning"
-        mBody = f"Hello {name},\n\nWelcome to JUPI Learning! Your account has been created with username as your email address.\n\nSign in to start your journey: https://jupilearning.app"
-        mText = f"From: {MAIL_ID}\nSubject: {mSubject}\n\n{mBody}"
-        mailServer.sendmail(MAIL_ID, [address], mText)
-        mailServer.close()
-    except:
-        print(f"Failed to send message to {address}")
+def send_mail(address,name):
+	try:
+		mailServer = smtplib.SMTP_SSL("smtp.gmail.com",465)
+		mailServer.ehlo()
+		mailServer.login(MAIL_ID,MAIL_PASSWORD)
+	except:
+		print("Failed connecting to gmail server")
+	try:
+		mSubject = "Welcome to JUPI-Learning"
+		mBody = "Hello {} to JUPI Learning, a groundbreaking platform dedicated to providing free, high-quality education to all.\
+			Your account has been created with username as your email address.\nSign in to start your journey\nhttps://jupilearning.app".format(name)
+		mText = """\
+From: {}
+Subject: {}
+{}""".format(MAIL_ID,mSubject,mBody)
+		mailServer.sendmail(MAIL_ID,[address,],mText)
+		mailServer.close()
+	except:
+		print("Failed to send message to {}".format(address))
 
-# Function to create a new user account
-def create_account(name, email, password):
-    accountExists = False
-    for i in db():
-        e = i[2]
-        if e == email:
-            accountExists = True
-            break
-    if not accountExists:
-        salt = gen_salt(32)
-        p = hashlib.sha256(str(password + salt).encode("UTF-8")).hexdigest()
-        cur.execute(f"insert into users (name, email, password, salt, prompt) values ('{name}', '{email.lower()}', '{p}', '{salt}', '')")
-        con.commit()
-        send_mail(email.lower(), name)
-        return True
-    else:
-        return False
+def create_account(name,email,password):
+	'''Create user account'''
+	accountExists = False
+	for i in db():
+		e = i[2]
+		if e == email:
+			accountExists = True
+			break
+	if not accountExists:
+		salt = gen_salt(32)
+		p = hashlib.sha256(str(password + salt).encode("UTF-8")).hexdigest()
+		cur.execute("insert into users (name,email,password,salt,prompt) values('{}','{}','{}','{}','')".format(name,email.lower(),p,salt))
+		con.commit()
+		send_mail(email.lower(),name)
+		return True
+	else:
+		return False
 
-# Function to authenticate user sign in
 def signin_account(data):
-    email = data["email"].lower()
-    password = data["password"]
-    matches = False
-    sessionID = ""
-    for i in db():
-        e, p, s = i[2], i[3], i[4]
-        pHash = hashlib.sha256(str(password + s).encode("UTF-8")).hexdigest()
-        if e == email and p == pHash:
-            matches = True
-            sessionID = fernet.encrypt(f"{email}<{pHash}".encode()).decode()
-            break
-    return (matches, sessionID)
+	'''Check whether the provided credentials are correct and authenticate'''
+	email = data["email"].lower()
+	password = data["password"]
+	matches = False
+	sessionID = ""
+	for i in db():
+		e,p,s = i[2],i[3],i[4]
+		pHash = hashlib.sha256(str(password + s).encode("UTF-8")).hexdigest()
+		if e == email and p == pHash:
+			matches = True
+			sessionID = fernet.encrypt("{}<>{}".format(email,pHash).encode()).decode()
+			break
+	return (matches,sessionID)
 
-# Function to validate user token
 def validate_token(token):
-    validated = False
-    username = ""
-    try:
-        email, password = fernet.decrypt(token.encode()).decode().split("<>")
-        for i in db():
-            u, e, p = i[1], i[2], i[3]
-            if e == email and p == password:
-                validated = True
-                username = u
-                break
-        return (validated, username)
-    except:
-        return (False, username)
+	'''Validate token in the client side'''
+	validated = False
+	username = ""
+	try:
+		email,password = fernet.decrypt(token.encode()).decode().split("<>")
+		for i in db():
+			u,e,p = i[1],i[2],i[3]
+			if e == email and p == password:
+				validated = True
+				username = u
+				break
+		return (validated,username)
+	except:
+		return (False,username)
 
-# Function to validate user signup data
+
+#Functions that handle requests
 def validate_signup(data):
-    errors = ""
-    if not str(data["name"]).replace(" ", "SEP").isalpha():
-        errors += "Name should only contain alphabets!. "
-    emailRegex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-    if not re.fullmatch(emailRegex, data["email"]):
-        errors += "Invalid email!. "
-    if len(data["name"]) > 256:
-        errors += "Name should only have 256 characters!. "
-    if len(data["email"]) > 320:
-        errors += "Email should only have 320 characters!. "
-    if errors == "":
-        acCreated = create_account(data["name"], data["email"], data["password"])
-        if not acCreated:
-            errors += "Account already exists!. "
-            return (False, errors)
-        return (True, errors)
-    else:
-        return (False, errors)
+	'''Check and report errors in user supplied information'''
+	'''Validates signup information provided by the user'''
+	errors = ""
+	#Name
+	if not str(data["name"]).replace(" ","SEP").isalpha():
+		errors += "Name should only contain alphabets!. "
+	#Email
+	emailRegex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+	if not re.fullmatch(emailRegex,data["email"]):
+		errors += "Invalid email!. "
 
-# End of Backend Code
+	#Checking length of data types
+	if len(data["name"]) > 256:
+		errors += "Name should only have 256 characters!. "
+	if len(data["email"]) > 320:
+		errors += "Email should only have 320 characters!. "
+
+	if errors == "":
+		acCreated = create_account(data["name"],data["email"],data["password"])
+		if not acCreated:
+			errors += "Account already exists!. "
+			return (False,errors)
+		return (True,errors)
+	else:
+		return (False,errors)
